cc3e8e01ce0940da4be08f6d88009657689c192c
LukasIs <lukas.pav0123@gmail.com>
2023-10-25 10:32:54 +0200

    moving point

--- res/js/main.js
+++ res/js/main.js
@@ -1,6 +1,26 @@
-let person = "Svatopluk";
-let age = 21;
+const point = document.getElementById("point");
+const startBtn = document.getElementById("startBtn");
+let gameInterval;
 
-const createSentence = (a, b) => `tvoje jmeno je ${a} a je ti ${b} let`;
+startBtn.onclick = () => {
+    startGameInterval(point);
+    hideElement(startBtn);
+}
 
-console.log(createSentence(person, age));
+const hideElement = (element) => {
+    element.style.display = "none";
+}
+
+const moveElement = (element, x, y) => {
+    element.style.left = `${x}px`;
+    element.style.top = `${y}px`;
+}
+
+const getRandNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const startGameInterval = (element) => {
+    clearInterval(gameInterval);
+    gameInterval = setInterval(() => {
+    moveElement(element, getRandNum(50, 1300), getRandNum(50, 600));
+}, 20)
+}
